5281c1ef0c45b75288bb8f8d9212808ab6d85058
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-11-08 08:56:50 +0100

    generate basic components for app

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-property-card',
+    templateUrl: 'property-card.component.html',
+    styleUrls: ['property-card.component.css']
+})
+
+export class PropertyCardComponent {}
