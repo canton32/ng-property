ab13c94da0d665e6c2d1a4a85070a9ab094723fa
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-11-09 03:49:02 +0100

    arrange repository folders

--- src/app/property/property-card/property-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-property-card',
-    templateUrl: 'property-card.component.html',
-    styleUrls: ['property-card.component.css']
-})
-
-export class PropertyCardComponent {}
