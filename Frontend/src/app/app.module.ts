f29fcdc0868ab9880ede4b79ea0865bb4cfee708
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-11-13 19:00:39 +0100

    add user registration form template

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -11,13 +11,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
-  {path: '**', component: PropertyListComponent}
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'user-register', component: UserRegisterComponent},
+  {path: '**', component: PropertyListComponent},
 ];
 
 @NgModule({
@@ -27,7 +31,9 @@ const appRoutes: Routes = [
     PropertyListComponent,
       NavBarComponent,
       AddPropertyComponent,
-      PropertyDetailComponent
+      PropertyDetailComponent,
+      UserLoginComponent,
+      UserRegisterComponent
    ],
   imports: [
     BrowserModule,
