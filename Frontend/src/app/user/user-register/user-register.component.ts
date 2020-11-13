a1af277f988b6a231605053e722fee1c91a11251
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-11-13 20:12:30 +0100

    implement reactive form for user-register with custom password match validator

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegisterComponent implements OnInit {
 
+  registrationForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.registrationForm  = new FormGroup({
+      userName: new FormControl('Mark', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    }, this.passwordMatchingValidator);
+  }
+
+  passwordMatchingValidator(fg: FormGroup): Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : {notMatched: true}
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm);
   }
 
 }
