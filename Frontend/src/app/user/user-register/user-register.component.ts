7a0ee94f5f1a4b65a31dfe46ebcf4ac16e5e9a32
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-11-15 00:35:21 +0100

    add validation/error messages to register form

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -26,6 +26,29 @@ export class UserRegisterComponent implements OnInit {
     return fg.get('password').value === fg.get('confirmPassword').value ? null : {notMatched: true};
   }
 
+
+  // getter methods for all form controls
+  get userName() {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
+  }
+  // -------------------
+
   onSubmit() {
     console.log(this.registrationForm);
   }
